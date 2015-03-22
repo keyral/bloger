31432f0559ae56f03fa1dc382eaa9a7db45de34a
simon <simon@hippocampe.Fr>
2015-03-22 23:47:55 +0100

    mise a jour du module contact avec routing plus form

---
+++ www/modules/custom/blog_contact/src/Form/ContactFormDefault.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\blog_contact\Form\ContactFormDefault.
+ */
+
+namespace Drupal\blog_contact\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class ContactFormDefault extends ConfigFormBase
+{
+
+
+
+
+
+  /**
+  * {@inheritdoc}
+  */
+  protected function getEditableConfigNames() {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormID() {
+    return 'contact_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('blog_contact.contact_form_config');
+    $form['lol'] = array(
+      '#type' => 'tel',
+      '#title' => $this->t('lol'),
+      '#description' => $this->t('lo'),
+      '#default_value' => $config->get('lol'),
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    /*
+    $this->config('blog_contact.contact_form_config')
+      ->set('lol', $form_state->getValue('lol'))
+    ->save();
+    */
+  }
+}
