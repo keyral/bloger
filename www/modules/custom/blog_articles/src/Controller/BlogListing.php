8a8f9b8e8704901bc8aead2c69b2af5510e8a507
simon <simon@hippocampe.Fr>
2015-04-18 15:51:16 +0200

    Mise a jour integral du blog

--- www/modules/custom/blog_articles/src/Controller/BlogListing.php
+++ www/modules/custom/blog_articles/src/Controller/BlogListing.php
@@ -28,16 +28,34 @@ class BlogListing extends ControllerBase {
   }
 
 
+  public function ViewAll() {
+    $listing = $this->QueryListing(true);
+    if(isset($listing)){
+      return array(
+        'content' => node_view_multiple($listing, "teaser"),
+        'pager' => array(
+          '#theme' => 'pager',
+        ),
+      );
+    }
+  }
 
-  private function QueryListing(){
+
+
+  private function QueryListing($all = False){
     $query = \Drupal::entityQuery('node');
     $query->condition('type', 'articles');
     $query->condition('status', 1);
     $query->sort('created', 'DESC');
-    $query->range(0,10);
+    if($all != False){
+      $query->pager(1);
+    }
+    else {
+      $query->range(0,10);
+    }
     $result = $query->execute();
 
-    if(isset($result[1])){
+    if(isset($result)){
       return $nodes = entity_load_multiple('node', $result);
     }
     return FALSE;
